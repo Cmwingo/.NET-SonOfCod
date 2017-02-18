4cb2d6772dbbdeaae254cb7eee381e20f4c17b43
Wingo <cmwingo@gmail.com>
2017-02-18 13:43:38 -0800

    Add Role controller and views

---
+++ src/SonOfCod/Controllers/RoleController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using SonOfCod.Models;
+using SonOfCod.ViewModels;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SonOfCod.Controllers
+{
+    public class RoleController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public RoleController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, ApplicationDbContext db, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _db = db;
+            _roleManager = roleManager;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var roles = _db.Roles.ToList();
+            return View(roles);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateRoleViewModel model)
+        {
+            var role = new IdentityRole { Name = model.Role };
+            IdentityResult result = await _roleManager.CreateAsync(role);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public async Task<IActionResult> Delete(string RoleName)
+        {
+            var thisRole = await _roleManager.FindByNameAsync(RoleName);
+            return View(thisRole);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(string RoleName)
+        {
+            var thisRole = await _roleManager.FindByNameAsync(RoleName);
+            _db.Roles.Remove(thisRole);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Edit(string RoleName)
+        {
+            var thisRole = await _roleManager.FindByNameAsync(RoleName);
+            return View(thisRole);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(IdentityRole role)
+        {
+            IdentityResult result = await _roleManager.UpdateAsync(role);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+    }
+}
---
+++ src/SonOfCod/ViewModels/CreateRoleViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SonOfCod.ViewModels
+{
+    public class CreateRoleViewModel
+    {
+        public string Role { get; set; }
+    }
+}
